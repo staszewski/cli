d3dec819cdd09abe6cafa6cad76b478f42641af1
staszewski <kamilstaszewski95@gmail.com>
2021-03-05 17:39:53 +0100

    Somehow working input / fetch

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
-use serde::{Deserialize, Serialize};
+use bat::PrettyPrinter;
+use std::io;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct User {
-    userId: i32,
-    id: i32,
-    title: String,
-    completed: bool,
+fn menu() -> u8 {
+    println!("\nMENU");
+    println!("----------------------------------------\n");
+    println!("Please, select an option:");
+    println!("\t 1. Creational patterns");
+    println!("\t 2. Behavioral patterns");
+    println!("\t 3. Structural patterns");
+    println!("\t 0. Exit");
+    let mut option: String = String::new();
+
+    io::stdin()
+        .read_line(&mut option)
+        .expect("Failed to read line");
+
+    let option = option.trim().parse::<u8>();
+
+    match option {
+        Ok(o) => return o,
+        Err(_) => {
+            eprintln!("ERROR: Please, type a number");
+            99
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp =
-        reqwest::blocking::get("https://jsonplaceholder.typicode.com/todos/1")?.json::<User>()?;
-    println!("{:?}", resp);
+    while {
+        let option: u8 = menu();
+
+        match option {
+            1 => {
+                let resp =
+        reqwest::blocking::get("https://raw.githubusercontent.com/lpxxn/rust-design-pattern/master/creational/singleton.rs")?.text()?;
+                let x = PrettyPrinter::new()
+                    .language("rust")
+                    .line_numbers(true)
+                    .input_from_bytes(resp.as_bytes())
+                    .print()
+                    .unwrap();
+                x
+            }
+            _ => {
+                println!("lala");
+                true
+            }
+        }
+    } {}
+
     Ok(())
 }
