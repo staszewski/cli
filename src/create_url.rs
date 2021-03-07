6067c8b07d6601d48b48268297215957543752e1
staszewski <kamilstaszewski95@gmail.com>
2021-03-07 13:39:01 +0100

    Append wip

--- src/create_url.rs
+++ src/create_url.rs
@@ -2,13 +2,14 @@ static INITIAL_URL: &str = "https://raw.githubusercontent.com/lpxxn/rust-design-
 pub struct CreateUrl<'a> {
     url: &'a str,
 }
-
+// trait?
 impl<'a> CreateUrl<'a> {
     pub fn init() -> CreateUrl<'static> {
         CreateUrl { url: INITIAL_URL }
     }
-    pub fn append(&self) {
-        todo!()
+    pub fn append(&self, path: &str) -> String {
+        let newstr = format!("{}{}", self.url, path);
+        newstr
     }
 }
 
@@ -19,5 +20,10 @@ mod tests {
     #[test]
     fn creates_correct_initial_url() {
         assert_eq!(CreateUrl::init().url, INITIAL_URL);
+    }
+
+    #[test]
+    fn appent_to_initial_url() {
+        assert_eq!(CreateUrl::init().append("lala"), INITIAL_URL);
     }
 }
