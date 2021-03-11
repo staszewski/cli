b3990c649ca055bc62310c21fea77ae7ff0e85bb
staszewski <kamilstaszewski95@gmail.com>
2021-03-11 19:38:53 +0100

    Create seperate function for each operation

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,21 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let website_address = &args[1];
     println!("{}", website_address);
-    fetch_links_from_website(website_address);
+    let document = fetch_links_from_website(website_address).unwrap();
+    let links = get_links_from_document(&document);
+    print!("{:?}", links)
 }
 
-fn fetch_links_from_website(address: &String) -> Result<(), Box<dyn std::error::Error>> {
+fn fetch_links_from_website(address: &String) -> Result<Document, Box<dyn std::error::Error>> {
     let resp = reqwest::blocking::get(address)?.text()?;
     let document = Document::from(resp.as_str());
+    Ok(document)
+}
+
+fn get_links_from_document<'a>(document: &'a Document) -> Vec<&'a str> {
     let mut links: Vec<&str> = vec![];
     for node in document.find(Name("a")) {
         links.push(node.attr("href").unwrap())
     }
-    println!("{:?}", links.len());
-    Ok(())
+    links
 }
