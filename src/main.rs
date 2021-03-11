29726241d0a20a38d590815ac775012f72396bc4
staszewski <kamilstaszewski95@gmail.com>
2021-03-11 19:16:47 +0100

    Fetch links

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,21 @@
-use bat::PrettyPrinter;
-use std::io;
-use url::UrlBuilder;
+use select::document::Document;
+use select::predicate::Name;
+use std::env;
 
-mod url;
-
-fn menu() -> u8 {
-    println!("\nMENU");
-    println!("----------------------------------------\n");
-    println!("Please, select an option:");
-    println!("\t 1. Creational patterns");
-    println!("\t 2. Behavioral patterns");
-    println!("\t 3. Structural patterns");
-    println!("\t 0. Exit");
-    let mut option: String = String::new();
-
-    io::stdin()
-        .read_line(&mut option)
-        .expect("Failed to read line");
-
-    let option = option.trim().parse::<u8>();
-
-    match option {
-        Ok(o) => return o,
-        Err(_) => {
-            eprintln!("ERROR: Please, type a number");
-            99
-        }
-    }
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let website_address = &args[1];
+    println!("{}", website_address);
+    fetch_links_from_website(website_address);
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    'cli: while {
-        let option: u8 = menu();
-
-        match option {
-            1 => {
-                let resp =
-        reqwest::blocking::get("https://raw.githubusercontent.com/lpxxn/rust-design-pattern/master/creational/singleton.rs")?.text()?;
-                PrettyPrinter::new()
-                    .language("rust")
-                    .line_numbers(true)
-                    .input_from_bytes(resp.as_bytes())
-                    .print()
-                    .unwrap();
-                break 'cli;
-            }
-            _ => {
-                println!("lala");
-                true
-            }
-        }
-    } {}
-
+fn fetch_links_from_website(address: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get(address)?.text()?;
+    let document = Document::from(resp.as_str());
+    let mut links: Vec<&str> = vec![];
+    for node in document.find(Name("a")) {
+        links.push(node.attr("href").unwrap())
+    }
+    println!("{:?}", links.len());
     Ok(())
 }
