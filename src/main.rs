2c7148d361fd51e6286278bdcfa85cfc6a1110a5
staszewski <kamilstaszewski95@gmail.com>
2021-03-14 13:49:49 +0100

    Working requesting

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let website_address = &args[1];
-    println!("{}", website_address);
     let document = fetch_links_from_website(website_address).unwrap();
     let links = get_links_from_document(&document);
-    print!("{:?}", links)
+    get_status_code_from_links(links, website_address);
 }
 
 fn fetch_links_from_website(address: &String) -> Result<Document, Box<dyn std::error::Error>> {
     let resp = reqwest::blocking::get(address)?.text()?;
     let document = Document::from(resp.as_str());
+    // Add match arm
     Ok(document)
 }
 
@@ -23,4 +23,31 @@ fn get_links_from_document<'a>(document: &'a Document) -> Vec<&'a str> {
         links.push(node.attr("href").unwrap())
     }
     links
+}
+
+fn validate_link(link: String, website_address: &String) -> String {
+    if link.contains("https") {
+        link
+    } else {
+        let relative_link = format!("{}{}", website_address.to_owned(), link.clone());
+        relative_link
+    }
+}
+
+fn get_status_code_from_links(
+    links: Vec<&str>,
+    base_link: &String,
+) -> Result<(), Box<(dyn std::error::Error + 'static)>> {
+    for link in links {
+        println!("{}", &validate_link(link.to_string(), base_link));
+        let resp = reqwest::blocking::get(&validate_link(link.to_string(), base_link))?;
+        if resp.status().is_success() {
+            println!("success!");
+        } else if resp.status().is_server_error() {
+            println!("server error!");
+        } else {
+            println!("Something else happened. Status: {:?}", resp.status());
+        }
+    }
+    Ok(())
 }
